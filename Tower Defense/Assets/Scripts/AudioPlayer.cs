73fdccfe1c1d9ef2851a2116909f4edc5e53ad54
juansheva <50511324+juansheva@users.noreply.github.com>
2022-08-04 19:47:25 +0700

    Completed

---
+++ Tower Defense/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    private static AudioPlayer _instance = null;
+    public static AudioPlayer Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioPlayer>();
+            }
+
+            return _instance;
+        }
+    }
+
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _audioClips;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void PlaySFX(string name)
+    {
+        AudioClip sfx = _audioClips.Find(s => s.name == name);
+
+        if (sfx == null)
+        {
+            return;
+        }
+
+        _audioSource.PlayOneShot(sfx);
+    }
+}
